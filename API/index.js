049922df67a87eba9714392f8cc36bac698565a8
Vinícius Carneiro <vinixiussouza@gmail.com>
2023-04-23 22:44:32 -0300

    directory change

--- API/index.js
+++ API/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import users from "./routes.js";
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
